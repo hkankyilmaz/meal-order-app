74e62304bc8be16daf71576e0dded24314479097
Hakan KARAYILMAZ <99439329+hkankyilmaz@users.noreply.github.com>
2022-10-26 12:58:44 +0300

    menu wrapper component

--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Carousel from "../../components/Corousel";
 import Campaigns from "../../components/Campaigns";
+import MenuWrapper from "../../components/product/MenuWrapper";
 
 const Index = () => {
   return (
     <div>
       <Carousel />
       <Campaigns />
+      <MenuWrapper />
     </div>
   );
 };
