542fd87630b2db8482d0765d38cb49f2268c3850
Hakan KARAYILMAZ <99439329+hkankyilmaz@users.noreply.github.com>
2022-10-28 14:54:41 +0300

    cart page total

--- components/Reservation.jsx
+++ components/Reservation.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Input from "./form/Input";
 import { useFormik } from "formik";
 import Title from "./ui/Title";
-import { reservationSchema } from "../scheme/reservation";
+import { reservationSchema } from "../schema/reservation";
 
 const Reservation = () => {
   const onSubmit = async (values, actions) => {
