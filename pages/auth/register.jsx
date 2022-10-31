7301feb47698e142be096ec03ffd9cf26481c768
Hakan KARAYILMAZ <99439329+hkankyilmaz@users.noreply.github.com>
2022-10-31 14:23:07 +0300

    register client process

--- pages/auth/register.jsx
+++ pages/auth/register.jsx
@@ -6,9 +6,17 @@ import { registerSchema } from "../../schema/register";
 
 const Register = () => {
   const onSubmit = async (values, actions) => {
-    await new Promise((resolve) => setTimeout(resolve, 4000));
-    actions.resetForm();
+    try {
+      const res = await axios.post(
+        `${process.env.NEXT_PUBLIC_API_URL}/users/register`,
+        values
+      );
+    } catch (err) {
+      console.log(err);
+    }
+    /* actions.resetForm(); */
   };
+
   const { values, errors, touched, handleSubmit, handleChange, handleBlur } =
     useFormik({
       initialValues: {
@@ -78,7 +86,9 @@ const Register = () => {
           ))}
         </div>
         <div className="flex flex-col w-full gap-y-3 mt-6">
-          <button className="btn-primary">REGISTER</button>
+          <button className="btn-primary" type="submit">
+            REGISTER
+          </button>
           <Link href="/auth/login">
             <span className="text-sm underline cursor-pointer text-secondary">
               Do you have a account?
